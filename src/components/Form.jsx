682022b9c5247e40500e70a679e5805d005bfa79
muhammadmario <acernitro58@gmail.com>
2023-12-12 12:30:27 +0700

    remove placeholder

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,7 +19,6 @@ const Form = () => {
           type="email"
           id="email"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-          placeholder="name@flowbite.com"
           required
         />
       </div>

ec09a317b1ea3e98569aa0a515c308bec7e8acee
muhammadmario <acernitro58@gmail.com>
2023-12-12 12:31:20 +0700

    change button label

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,7 +41,7 @@ const Form = () => {
           type="submit"
           className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
         >
-          Submit
+          Login
         </button>
       </div>
     </form>
